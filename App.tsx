3ae9cbfc770a1a3fd658ffee9686ed545e63f37e
Thiago Novato <thiagonovato@gmail.com>
2022-07-25 12:31:56 -0400

    Resolvendo o problema da splash

--- App.tsx
+++ App.tsx
@@ -5,16 +5,21 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
   });
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
